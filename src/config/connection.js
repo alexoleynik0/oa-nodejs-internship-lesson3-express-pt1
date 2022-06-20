de8842faf1823155438d3c893370da71dd2e7600
Alex Oleynik <oleynik.webdev@gmail.com>
2022-06-21 02:40:14 +0300

    graceful start and shutdown for pm2 added;

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,8 +1,23 @@
 class Connection {
   static init(app, port) {
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       console.log(`app is running at http://localhost:${port}`);
+
+      // Graceful Start (pm2)
+      if (typeof process.send === 'function') {
+        process.send('ready');
+      }
     });
+
+    // Graceful Shutdown (pm2, cmd+c)
+    const gracefulShutdown = (signal) => () => {
+      console.log(`${signal} signal received: closing HTTP server`);
+      server.close(() => {
+        console.log('HTTP server closed');
+      });
+    };
+    process.on('SIGINT', gracefulShutdown('SIGINT'));
+    process.on('SIGTERM', gracefulShutdown('SIGTERM'));
   }
 }
 
